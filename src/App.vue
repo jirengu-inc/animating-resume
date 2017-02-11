1bf48c1465abccf14c9e4f9169ba16a37b3c3559
frankfang <frankfang1990@gmail.com>
2017-02-11 22:33:22 +0800

    基本原理搞定

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <Editor :code="html"></Editor>
+    <Preview :html="html"></Preview>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Editor from './components/Editor'
+import Preview from './components/Preview'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Editor, Preview
+  },
+  data(){
+    return {
+      html: '',
+      fullHtml:
+`<!--
+大家好，我是方方 
+二月了，好多公司都在招聘，我想，我也应该写一份简历呀。
+说做就做！
+-->
+
+<style>
+  html{
+    background: hsl(155,50%,50%);
+  }
+</style>
+
+<h1>方方</h1>
+<h2>前端开发工程师</h2>
+
+
+`,
+    }
+  },
+  created(){
+    let length = this.fullHtml.length
+    let inteval = 50
+    let a = ()=>{
+      if(this.html.length < length){
+        this.html = this.fullHtml.substring(0, this.html.length+1)
+        setTimeout(a,inteval)
+      }
+    }
+    setTimeout(a,inteval)
   }
 }
 </script>
@@ -21,8 +56,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Editor.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <pre v-html="highlightedCode"></pre>
+  </div>
+</template>
+
+<script>
+import Prism from 'prismjs'
+export default {
+  name: 'Editor',
+  props: ['code'],
+  computed: {
+    highlightedCode: function(){
+      return Prism.highlight(this.code, Prism.languages.html)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
