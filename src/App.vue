610fbc6eacff316641224a0009857576b784cf2c
frankfang <frankfang1990@gmail.com>
2017-02-12 19:32:50 +0800

    re-indent

--- src/App.vue
+++ src/App.vue
@@ -23,63 +23,67 @@
         enableHtml: false,
         fullStyle: [
           `  /*
-  * Inspired by http://strml.net/
-  * 大家好，我是方方 
-  * 二月了，好多公司都在招聘，你是不是也在准备简历呀。
-  * 说做就做，我也来写一份简历！
-  */
+* Inspired by http://strml.net/
+* 大家好，我是方方 
+* 二月了，好多公司都在招聘，你是不是也在准备简历呀。
+* 说做就做，我也来写一份简历！
+*/
 
-  /* 首先给所有元素加上过渡效果 */
-  * {
-    transition: all .3s;
-  }
-  /* 白色背景太单调了，我们来点背景 */
-  html {
-    color: rgb(222,222,222); background: rgb(0,43,54);
-  }
-  /* 文字离边框太近了 */
-  .styleEditor {
-    padding: .5em;
-    border: 1px solid;
-    margin: .5em;
-    overflow: auto;
-    width: 45vw; height: 90vh;
-  }
-  /* 代码高亮 */
-  .token.selector{ color: rgb(133,153,0); }
-  .token.property{ color: rgb(187,137,0); }
-  .token.punctuation{ color: yellow; }
-  .token.function{ color: rgb(42,161,152); }
+/* 首先给所有元素加上过渡效果 */
+* {
+  transition: all .3s;
+}
+/* 白色背景太单调了，我们来点背景 */
+html {
+  color: rgb(222,222,222); background: rgb(0,43,54);
+}
+/* 文字离边框太近了 */
+.styleEditor {
+  padding: .5em;
+  border: 1px solid;
+  margin: .5em;
+  overflow: auto;
+  width: 45vw; height: 90vh;
+}
+/* 代码高亮 */
+.token.selector{ color: rgb(133,153,0); }
+.token.property{ color: rgb(187,137,0); }
+.token.punctuation{ color: yellow; }
+.token.function{ color: rgb(42,161,152); }
 
-  /* 加点 3D 效果呗 */
-  html{
-    perspective: 1000px;
-    min-height: 100vh;
-  }
-  .styleEditor {
-    position: fixed; left: 0; top: 0; 
-    transition: none;
-    transform: rotateY(10deg) translateZ(-100px) ;
-  }
+/* 加点 3D 效果呗 */
+html{
+  perspective: 1000px;
+  min-height: 100vh;
+}
+.styleEditor {
+  position: fixed; left: 0; top: 0; 
+  transition: none;
+  transform: rotateY(10deg) translateZ(-100px) ;
+}
 
-  /* 接下来我给自己准备一个编辑器 */
-  .resumeEditor{
-    position: fixed; right: 0; top: 0;
-    padding: .5em;  margin: .5em;
-    width: 48vw; height: 90vh; 
-    border: 1px solid;
-    background: white; color: #222;
-    overflow: auto;
-  }
-  /* 好了，我开始写简历了 */
+/* 接下来我给自己准备一个编辑器 */
+.resumeEditor{
+  position: fixed; right: 0; top: 0;
+  padding: .5em;  margin: .5em;
+  width: 48vw; height: 90vh; 
+  border: 1px solid;
+  background: white; color: #222;
+  overflow: auto;
+}
+/* 好了，我开始写简历了 */
+
+
 `,
-          `  /* 这个简历好像差点什么
-  * 对了，这是 Markdown 格式的，我需要变成对 HR 更友好的格式
-  * 简单，用开源工具翻译成 HTML 就行了
-  */
+          `
+/* 这个简历好像差点什么
+ * 对了，这是 Markdown 格式的，我需要变成对 HR 更友好的格式
+ * 简单，用开源工具翻译成 HTML 就行了
+ */
 `
           ,
-          `  /* 再对 HTML 加点样式 */
+          `
+/* 再对 HTML 加点样式 */
 .resumeEditor{
   padding: 2em;
 }
