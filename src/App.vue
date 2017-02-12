5512deb2b6c2d84c5b488aaa4ac46d68eac4ba97
frankfang <frankfang1990@gmail.com>
2017-02-12 10:55:23 +0800

    将 Editor 重命名为 StyleEditor

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div id="app">
-    <Editor :code="html"></Editor>
-    <Preview :html="html"></Preview>
+    <StyleEditor class="styleEditor" :code="currentStyle"></StyleEditor>
+    <StylePreview class="stylePreview" :html="currentStyle"></StylePreview>
   </div>
 </template>
 
 <script>
-import Editor from './components/Editor'
-import Preview from './components/Preview'
+import StyleEditor from './components/StyleEditor'
+import StylePreview from './components/StylePreview'
+import './assets/reset.css'
 
 export default {
   name: 'app',
   components: {
-    Editor, Preview
+    StyleEditor, StylePreview
   },
   data(){
     return {
-      html: '',
-      fullHtml:
+      currentStyle: '',
+      fullStyle:
 `<!--
 大家好，我是方方 
 二月了，好多公司都在招聘，我想，我也应该写一份简历呀。
@@ -25,24 +26,27 @@ export default {
 -->
 
 <style>
+  * {
+    transition: all 1s;
+  }
   html{
     background: hsl(155,50%,50%);
   }
+  .styleEditor{
+    padding: .5em;
+    margin: .5em;
+    border: 1px solid;
+  }
 </style>
-
-<h1>方方</h1>
-<h2>前端开发工程师</h2>
-
-
 `,
     }
   },
   created(){
-    let length = this.fullHtml.length
+    let length = this.fullStyle.length
     let inteval = 50
     let a = ()=>{
-      if(this.html.length < length){
-        this.html = this.fullHtml.substring(0, this.html.length+1)
+      if(this.currentStyle.length < length){
+        this.currentStyle = this.fullStyle.substring(0, this.currentStyle.length+1)
         setTimeout(a,inteval)
       }
     }
@@ -51,11 +55,11 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+<style scoped>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #222;
+  }
 </style>
