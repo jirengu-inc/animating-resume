3697f2f0b2acb2571d6bd0806ab304f21515a82b
frankfang <frankfang1990@gmail.com>
2017-02-12 14:07:38 +0800

    支持多个 style

--- src/App.vue
+++ src/App.vue
@@ -19,45 +19,21 @@
     data() {
       return {
         currentStyle: '',
-        fullStyle: `
+        fullStyle: [`
 <!--
 大家好，我是方方 
 二月了，好多公司都在招聘，我想，我也应该写一份简历呀。
 说做就做！
 -->
-
+`, `
 <style>
-  * {
-    transition: all 1s;
-  }
-  
-  html {
-    background: hsl(155, 50%, 50%);
-  }
-  
-  .styleEditor {
-    padding: .5em;
-    margin: .5em;
-    border: 1px solid;
-  }
+* {border-color: red;}
 </style>
-`,
+`],
         currentMarkdown: '',
         fullMarkdown: `
 # 方方
 
-资深前端开发工程师
-
-# 技能
-
-* 前端开发
-* Rails 开发
-* Node.js 开发
-* 技术吹水
-
-# 联系方式
-
-保密
 `
       }
     },
@@ -65,21 +41,25 @@
       this.progressivelyShowStyle()
     },
     methods: {
-      async progressivelyShowStyle(done) {
-        let length = this.fullStyle.length
+      async progressivelyShowStyle() {
         let inteval = 50
-        let showStyle = (async function () {
+        let showStyle = (async function (n) {
+          let style = this.fullStyle[n]
+          if (!style) { return }
+          let length = this.fullStyle.filter((_, index) => index <= n).map((item) => item.length).reduce((p, c) => p + c, 0)
+          let prefixLength = length - style.length
           if (this.currentStyle.length < length) {
-            this.currentStyle = this.fullStyle.substring(0, this.currentStyle.length + 1)
-            setTimeout(showStyle, inteval)
+            let l = this.currentStyle.length - prefixLength
+            this.currentStyle += style.substring(l, l + 1) || ' '
+            setTimeout(showStyle, inteval, n)
           } else {
-            await this.progressivelyShowResume()
-            console.log(1)
+            if (n === 0) { await this.progressivelyShowResume() }
+            showStyle(n + 1)
           }
         }).bind(this)
-        showStyle()
+        showStyle(0)
       },
-      progressivelyShowResume(done) {
+      progressivelyShowResume() {
         return new Promise((resolve, reject) => {
           let length = this.fullMarkdown.length
           let inteval = 50
