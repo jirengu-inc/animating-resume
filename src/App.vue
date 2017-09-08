f250d882ca9c42fc123c16206682fbdf30c2f38e
frankfang <frankfang1990@gmail.com>
2017-09-08 23:07:07 +0800

    build

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@
         fullStyle: [
           `/*
 * Inspired by http://strml.net/
-* 大家好，我是方方 
+* 大家好，我是方方
 * 二月了，好多公司都在招聘，你是不是也在准备简历呀。
 * 说做就做，我也来写一份简历！
 */
@@ -36,7 +36,7 @@
 }
 /* 白色背景太单调了，我们来点背景 */
 html {
-  color: rgb(222,222,222); background: rgb(0,43,54); 
+  color: rgb(222,222,222); background: rgb(0,43,54);
 }
 /* 文字离边框太近了 */
 .styleEditor {
@@ -58,8 +58,8 @@ html{
           perspective: 1000px;
 }
 .styleEditor {
-  position: fixed; left: 0; top: 0; 
-  -webkit-transition: none; 
+  position: fixed; left: 0; top: 0;
+  -webkit-transition: none;
   transition: none;
   -webkit-transform: rotateY(10deg) translateZ(-100px) ;
           transform: rotateY(10deg) translateZ(-100px) ;
@@ -69,7 +69,7 @@ html{
 .resumeEditor{
   position: fixed; right: 0; top: 0;
   padding: .5em;  margin: .5em;
-  width: 48vw; height: 90vh; 
+  width: 48vw; height: 90vh;
   border: 1px solid;
   background: white; color: #222;
   overflow: auto;
@@ -106,8 +106,8 @@ html{
   counter-reset: section;
 }
 .resumeEditor ol li::before {
-  counter-increment: section;            
-  content: counters(section, ".") " ";  
+  counter-increment: section;
+  content: counters(section, ".") " ";
   margin-right: .5em;
 }
 .resumeEditor blockquote {
@@ -225,11 +225,14 @@ html{
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
-  
+
   html {
     min-height: 100vh;
   }
-  
+  pre{
+    max-height: 3000px;
+  }
+
   * {
     -webkit-transition: all .3s;
     transition: all .3s;
